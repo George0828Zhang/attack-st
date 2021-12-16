fb5df90b64af4c2972d45fdac8ba74e0fec6e65d
Chih-Chiang Chang <r09922057@ntu.edu.tw>
2021-12-17 01:38:56 +0800

    fixed eval

--- eval/eval_asr.sh
+++ eval/eval_asr.sh
@@ -1,21 +1,57 @@
 #!/usr/bin/env bash
-MODEL=${1:-s2t_ctc_asr_1}
-SPLIT=${2:-dev}
-EXP=../exp
+# credits: https://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
+POSITIONAL=()
+while [[ $# -gt 0 ]]; do
+  key="$1"
+
+  case $key in
+    -m|--model)
+      MODEL="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -s|--source)
+      SRC_FILE="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -t|--target)
+      TGT_FILE="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -e|--exp)
+      EXP="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    *)    # unknown option
+      POSITIONAL+=("$1") # save it in an array for later
+      shift # past argument
+      ;;
+  esac
+done
+
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+# defaults
+MODEL=${MODEL:-s2t_ctc_asr_1}
+SRC_FILE=${SRC_FILE:-data/test.wav_list}
+TGT_FILE=${TGT_FILE:-data/test.en}
+EXP=${EXP:-../exp}
 . ${EXP}/data_path.sh
-# DATA=${DATA_ROOT}/joint
 CONF=$DATA/config_asr.yaml
 CHECKDIR=${EXP}/checkpoints/${MODEL}
-RESULTS=${SPLIT}_results/${MODEL}
+RESULTS=$(dirname ${SRC_FILE})_results/${MODEL}
 AVG=true
 
 EXTRAARGS=""
 
 if [[ $AVG == "true" ]]; then
     CHECKPOINT_FILENAME=avg_best_5_checkpoint.pt
-    # python ../scripts/average_checkpoints.py \
-    #   --inputs ${CHECKDIR} --num-best-checkpoints 5 \
-    #   --output "${CHECKDIR}/${CHECKPOINT_FILENAME}"
+    python ../scripts/average_checkpoints.py \
+      --inputs ${CHECKDIR} --num-best-checkpoints 5 \
+      --output "${CHECKDIR}/${CHECKPOINT_FILENAME}"
 else
     CHECKPOINT_FILENAME=checkpoint_best.pt
 fi
@@ -32,9 +68,9 @@ lang=${SRC}
 # tail +2 ${tsv} | cut -f4 > ${RESULTS}/refs.${lang}
 # cat ${RESULTS}/feats.${lang} | \
 
-cp ./data/${SPLIT}.en ${RESULTS}/refs.${lang}
+cp ${TGT_FILE} ${RESULTS}/refs.${lang}
 
-cat ./data/${SPLIT}.wav_list | \
+cat ${SRC_FILE} | \
 python -m fairseq_cli.interactive ${DATA} --user-dir ${USERDIR} \
     --config-yaml ${CONF} \
     --gen-subset ${SPLIT}_st_${SRC}_${TGT} \
@@ -46,7 +82,7 @@ python -m fairseq_cli.interactive ${DATA} --user-dir ${USERDIR} \
     ${EXTRAARGS} | \
 grep -E "D-[0-9]+" | \
 cut -f3 > ${RESULTS}/hyps.${lang}
-echo "# evaluating WER"
-wer -a ${RESULTS}/refs.${lang} ${RESULTS}/hyps.${lang} | tee ${RESULTS}/score.${lang}
+# echo "# evaluating WER"
+# wer -a ${RESULTS}/refs.${lang} ${RESULTS}/hyps.${lang} | tee ${RESULTS}/score.${lang}
 echo "# evaluating CER"
 wer -i -a <(char ${RESULTS}/refs.${lang}) <(char ${RESULTS}/hyps.${lang}) | tee ${RESULTS}/char_score.${lang}
--- eval/filter_examples.sh
+++ eval/filter_examples.sh
@@ -1,11 +1,50 @@
 #!/usr/bin/env bash
-. ../exp/data_path.sh
-FILE=${1:-"test_results/s2t_ctc_asr_1/char_score.en"}
-NUM=${2:-500}
-SET=${3:-"test"}
-OUT=${4:-"test2"}
-DIR=./data
-BEST=$DIR/$SET.best_id
+# credits: https://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
+POSITIONAL=()
+while [[ $# -gt 0 ]]; do
+  key="$1"
+
+  case $key in
+    -m|--model)
+      MODEL="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -s|--split)
+      SPLIT="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -o|--out)
+      OUT="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -n|--num)
+      NUM="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -e|--exp)
+      EXP="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    *)    # unknown option
+      POSITIONAL+=("$1") # save it in an array for later
+      shift # past argument
+      ;;
+  esac
+done
+
+set -- "${POSITIONAL[@]}" # restore positional parameters
+EXP=${EXP:-../exp}
+. ${EXP}/data_path.sh
+FILE=${FILE:-"test.wav_list_results/s2t_ctc_asr_1/char_score.en"}
+NUM=${NUM:-500}
+SPLIT=${SPLIT:-"test"}
+OUT=${OUT:-"benign"}
+BEST=${SPLIT}.best_id
 
 # get list of best wer line numbers
 grep -E "SENTENCE|Errors" $FILE | \
@@ -17,6 +56,7 @@ grep -E "SENTENCE|Errors" $FILE | \
     cut -d' ' -f2 > $BEST
 
 # get these examples as test2 set
+mkdir -p $OUT
 for suf in wav_list $SRC $TGT; do
-    awk 'NR==FNR{ a[$1]; next }FNR in a' $BEST $DIR/$SET.$suf > $DIR/$OUT.$suf
+    awk 'NR==FNR{ a[$1]; next }FNR in a' $BEST data/$SPLIT.$suf > $OUT/test.$suf
 done
